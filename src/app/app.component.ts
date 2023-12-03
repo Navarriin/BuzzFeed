482b8174300e856bd9338164e63005404e4b828c
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-03 16:26:24 -0300

    mais ajustes iniciais e criaçao de novo component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { QuizzComponent } from './components/quizz/quizz.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, QuizzComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'buzzFeed';
