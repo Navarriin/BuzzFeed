1ddd865241b4bcc643e0035e1aaa1c4e7b628a95
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-03 18:19:11 -0300

    deixando as perguntas dinamicas

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,12 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quizz.component.html',
-  styleUrl: './quizz.component.scss'
+  styleUrl: './quizz.component.scss',
 })
 export class QuizzComponent {
+  title: string = '';
 
+  questions: any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answerSelected: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex!: number;
+
+  finished: boolean = false;
+
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.loadQuestions();
+
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  nextQuestion(alias: string): void {
+    if (this.questionIndex < this.questionMaxIndex) this.questionIndex += 1;
+    this.answers.push(alias);
+    this.loadQuestions();
+  }
+
+  loadQuestions(): void {
+    this.questionSelected = this.questions[this.questionIndex];
+
+    if (this.questionIndex === this.questionMaxIndex) this.finished = true;
+  }
+
+  result(): void {}
 }
